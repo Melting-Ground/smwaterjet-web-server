c155490f5f4800843d9f0d5659f670ef8cef8f7e
UiChan Lee <junglkls1234@gmail.com>
2026-01-21 23:50:04 +0900

    Feat: reportAPI 제거

--- src/main/index.js
+++ src/main/index.js
@@ -9,7 +9,6 @@ const exceptionHandler = require('@middlewares/exception-handler');
 const setClientIP = require('@middlewares/client-ip');
 const adminRoutes = require('@routes/admin-routes');
 const photoRoutes = require('@routes/photo-routes');
-const reportRoutes = require('@routes/report-routes');
 const inquiryRoutes = require('@routes/inquiry-routes');
 const noticeRoutes = require('@routes/notice-routes');
 const turnstileRoutes = require('@routes/turnstile-routes');
@@ -29,7 +28,6 @@ app.use('/public', express.static(uploadsPath));
 app.use('/turnstile', turnstileRoutes)
 app.use('/admins', adminRoutes);
 app.use('/photos', photoRoutes);
-app.use('/reports', reportRoutes);
 app.use('/inquiries', inquiryRoutes);
 app.use('/notices', noticeRoutes);
 
